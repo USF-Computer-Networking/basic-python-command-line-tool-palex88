671501d72f79392ec734b9632bd6a178890a9b54
Alex Thompson <awt1088@gmail.com>
2018-02-12 10:31:15 -0800

    documentation changes

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 #!usr/bin/env/python
-'''
-    File:           main.py
-    Author:         Alex Thompson
-    Date Created:   2018-02-12
-    Date Modified:  2018-02-12
-    Python Version: 3.6
-'''
+
+# File:           main.py
+# Author:         Alex Thompson
+# Github:         palex88
+# Date Created:   2018-02-12
+# Date Modified:  2018-02-12
+# Python Version: 3.6
+
+
+import argparse
+
+
+def run():
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
 
 if __name__ == '__main__':
     pass
